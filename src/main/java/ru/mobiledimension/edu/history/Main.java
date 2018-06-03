5247a54d39f12952ae8df8664aabaf65ae492b2a
Yury <z-gys@yandex.ru>
2018-06-04 00:35:43 +0300

    stage 1, added Main

---
+++ src/main/java/ru/mobiledimension/edu/history/Main.java
@@ -0,0 +1,11 @@
+package ru.mobiledimension.edu.history;
+
+/**
+ * @author U.Goryntsev 04.06.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        IRobot iRobot = new IRobot();
+        iRobot.cleanTheRoom();
+    }
+}
